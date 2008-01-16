3d1e91d9da2182c54614488606c542e2e15eb1e1
Joao Sergio Lopez Silva <jslopez@csrg.inf.utfsm.cl>
2008-01-16 12:41:04 -0300

    Initial commit.

---
+++ src/client.cpp
@@ -0,0 +1,16 @@
+/**
+ * \mainpage Meade LX200 GPS client
+ */
+
+/**
+ * \file main.cpp
+ * \author Joao Lopez <jslopez@csrg.inf.utfsm.cl>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int args, char *argv[])
+{
+	exit(EXIT_SUCCESS);
+}
